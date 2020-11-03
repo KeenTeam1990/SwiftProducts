a977f42ad3d3e02c9444be3a8a38a930bd086509
KeenTeam <812144991@qq.com>
2020-11-03 11:37:13 +0800

    Add files via upload
    keenteam

---
+++ NSDate-Extension.swift
@@ -0,0 +1,42 @@
+
+//
+//  NSDate-Extension.swift
+//  DYLive
+//
+//  Created by Mr_Han on 2019/4/17.
+//  Copyright © 2019 Mr_Han. All rights reserved.
+//  CSDN <https://blog.csdn.net/u010960265>
+//  GitHub <https://github.com/HanQiGod>
+//
+
+import UIKit
+
+
+extension NSDate {
+    
+    class func getCurrentTime() -> String {
+        
+        let nowDate = NSDate()
+        
+        let interval = Int(nowDate.timeIntervalSince1970)
+        
+        return "\(interval)"
+        
+    }
+    
+}
+
+extension NSDate {
+    
+    class func getCurrentTime() -> String {
+        
+        let nowDate = NSDate()
+        let interval = Int(nowDate.timeIntervalSince1970)
+        
+        return String(interval)
+        
+        return "\(interval)"
+        
+    }
+    
+}
